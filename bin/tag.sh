1f1815344e820715f120795fe47abc2661f3bcc0
AndreyLebedenko <andrey.lebedenko@gmail.com>
2018-10-18 12:43:26 +0100

    Added required shell scripts. Updated .gitignore to push them.

---
+++ bin/tag.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script to tag the GIT repository with a specific version taken from the POM file
+
+#set -x
+
+function slack {
+  local SLACK_URL="https://hooks.slack.com/services/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";
+  local PAYLOAD="payload={\"channel\": \"dev_streaming_notify\", \"text\":\" $1 \", \"username\": \"Travis\", \"icon_url\": \"https://fst.slack-edge.com/66f9/img/services/travis_36.png\"}"
+  curl -X POST --data-urlencode "$PAYLOAD" $SLACK_URL
+}
+
+# Get VERSION from top level POM
+VERSION_POM=$( ./mvnw help:evaluate -Dexpression=project.version | grep -v '\[.*' | tail -n1 )
+
+# Get ARTIFACT_ID from top level POM
+ARTIFACT_ID_POM=$( ./mvnw help:evaluate -Dexpression=project.artifactId | grep -v '\[.*' | tail -n1 )
+
+# Setup Git Configuration
+git config --global user.email "build@travis-ci.com"
+git config --global user.name "Travis CI"
+
+./mvnw scm:tag && slack "Tagged $ARTIFACT_ID_POM with version $VERSION_POM"
---
+++ bin/version.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Script to create a semver version as MAJOR.MINOR.PATCH (https://semver.org/) where:
+#   - MAJOR.MINOR is taken from top level POM
+#   - PATCH is different for each branch:
+#       - master branch, will be based on latest tag like MAJOR.MINOR.PREVIOUS_PATCH and return PREVIOUS_PATCH + 1
+#       - other branches, will be the Git Hash
+
+#set -x
+
+# Get PATCH from git latest tag starting with major.minor, otherwise 0
+function getPatchFromGitTag {
+    local VERSION_TAG=$( git tag -l "${MAJOR}.${MINOR}*" | sort -t . -k 3 -g | tail -n1 )
+    local VERSION_TAG_BITS=(${VERSION_TAG//./ })
+
+    local PATCH=0
+    if [ ! -z "${VERSION_TAG_BITS[2]}" ]
+    then
+        PATCH=$((${VERSION_TAG_BITS[2]} + 1))
+    fi
+
+    echo "$PATCH"
+}
+
+function getPatchFromGitHash {
+  local GIT_HASH=$( git rev-parse HEAD | cut -c 1-7 )
+  local PATCH="0-$GIT_HASH"
+  echo "$PATCH"
+}
+
+# Extract branch from Travis CI
+if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]
+then
+  BRANCH=$TRAVIS_BRANCH
+else
+  BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+fi
+
+# Get MAJOR and MINOR from top level POM
+VERSION_POM=$( ./mvnw help:evaluate -Dexpression=project.version | grep -v '\[.*' | tail -n1 )
+VERSION_POM_BITS=(${VERSION_POM//./ })
+
+MAJOR=${VERSION_POM_BITS[0]}
+MINOR=${VERSION_POM_BITS[1]}
+
+# Get PATCH depending on branch
+if [[ "$BRANCH" = "master" ]]
+then
+    PATCH=$(getPatchFromGitTag)
+else
+    PATCH=$(getPatchFromGitHash)
+fi
+
+# Set the new version in POM
+./mvnw versions:set -DgenerateBackupPoms=false -DnewVersion="${MAJOR}.${MINOR}.${PATCH}"
