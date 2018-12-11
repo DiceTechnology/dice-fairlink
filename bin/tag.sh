c42fa4060c4971c842447c5e8a30cdbd1e2d1b7e
AndreyLebedenko <andrey.lebedenko@gmail.com>
2018-12-11 14:20:15 +0000

    Better use of slack url and API key

--- bin/tag.sh
+++ bin/tag.sh
@@ -5,7 +5,7 @@
 
 function slack {
   local PAYLOAD="payload={\"channel\": \"dice-opensource\", \"text\":\" $1 \", \"username\": \"Travis\", \"icon_url\": \"https://fst.slack-edge.com/66f9/img/services/travis_36.png\"}"
-  curl -X POST --data-urlencode "$PAYLOAD" $encrypted_SLACK_URL
+  curl -X POST --data-urlencode "$PAYLOAD" $encrypted_SLACK_URL/$encrypted_SLACK_ROOM_API_KEY
 }
 
 # Get VERSION from top level POM
