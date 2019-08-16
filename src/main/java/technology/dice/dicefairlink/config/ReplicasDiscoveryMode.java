a39f7184b964dbdc46cd5a5e64fee7755569a8dd
Gonçalo Luiz <goncalo.luiz@gmail.com>
2019-08-16 22:42:21 +0100

    more elegant resolution of string

--- src/main/java/technology/dice/dicefairlink/config/ReplicasDiscoveryMode.java
+++ src/main/java/technology/dice/dicefairlink/config/ReplicasDiscoveryMode.java
@@ -1,6 +1,5 @@
 package technology.dice.dicefairlink.config;
 
-import java.util.Arrays;
 import java.util.Optional;
 
 public enum ReplicasDiscoveryMode {
@@ -8,8 +7,6 @@ public enum ReplicasDiscoveryMode {
   SQL_MYSQL;
 
   public static Optional<ReplicasDiscoveryMode> fromStringInsensitive(String candidate) {
-    return Arrays.stream(ReplicasDiscoveryMode.values())
-        .filter(mode -> mode.toString().equalsIgnoreCase(candidate))
-        .findAny();
+    return Optional.of(ReplicasDiscoveryMode.valueOf(candidate.toUpperCase()));
   }
 }
