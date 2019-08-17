8b28549cb16af55923d3d0cea2bfcf1db20b624a
Gonçalo Luiz <goncalo.luiz@gmail.com>
2019-08-17 01:19:49 +0100

    added copyright headers

--- src/main/java/technology/dice/dicefairlink/discovery/tags/awsapi/ResourceGroupApiTagDiscovery.java
+++ src/main/java/technology/dice/dicefairlink/discovery/tags/awsapi/ResourceGroupApiTagDiscovery.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2018 - present by Dice Technology Ltd.
+ *
+ * Please see distribution for license.
+ */
 package technology.dice.dicefairlink.discovery.tags.awsapi;
 
 import software.amazon.awssdk.services.resourcegroupstaggingapi.ResourceGroupsTaggingApiClient;
