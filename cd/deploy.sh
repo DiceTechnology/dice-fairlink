b491c935b9e56436e2a02eb98371957261010924
Miguel Pontes <mpontes@users.noreply.github.com>
2021-04-21 09:15:16 +0100

    chore: Add missing gpg and nexus-staging maven plugins

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     gpg --batch --fast-import gpg.asc
-    mvn deploy --settings cd/mvnsettings.xml
+    mvn deploy -P publish --settings cd/mvnsettings.xml
 fi
